035353ad55cfe20f4a2ea0b10b86e55e3b3e92d7
arsyee <tibi@fallen.hu>
2017-12-10 10:17:29 +0100

    restructured, made less verbose and added solution as test

--- src/hu/fallen/adventofcode/solutions/Solution10.java
+++ src/hu/fallen/adventofcode/solutions/Solution10.java
@@ -2,8 +2,11 @@ package hu.fallen.adventofcode.solutions;
 
 public class Solution10 {
 
+    static boolean debug = true;
+    
     public static void printSolution() {
         System.out.println(calculate(256, 63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24));
+        System.out.println(calculate2(256, "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24"));
     }
 
     public static int calculate(int length, int... input) {
@@ -12,6 +15,18 @@ public class Solution10 {
             string[i] = i;
         }
         
+        debug = false;
+        hashRound(length, string, input);
+        debug = true;
+        
+        return string[0]*string[1];
+    }
+
+    public static int calculate2(int length, String input) {
+        return 0;
+    }
+    
+    private static void hashRound(int length, int[] string, final int[] input) {
         int position = 0;
         int skip = 0;
         
@@ -31,11 +46,10 @@ public class Solution10 {
             skip++;
             print(position, string);
         }
-        
-        return string[0]*string[1];
     }
 
     private static void print(int position, int[] string) {
+        if (!debug) return;
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < string.length; ++i) {
             if (i > 0) result.append(" ");
--- src/hu/fallen/adventofcode/tests/Solution10Test.java
+++ src/hu/fallen/adventofcode/tests/Solution10Test.java
@@ -11,6 +11,7 @@ public class Solution10Test {
     @Test
     public void test() {
         assertEquals(12, Solution10.calculate(5, 3, 4, 1, 5));
+        assertEquals(4480, Solution10.calculate(256, 63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24));
     }
 
 }
