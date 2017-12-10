f03686210449c83473aa451f6a611f8c71449454
arsyee <tibi@fallen.hu>
2017-12-10 11:00:39 +0100

    day 10 solution 2

--- src/hu/fallen/adventofcode/solutions/Solution10.java
+++ src/hu/fallen/adventofcode/solutions/Solution10.java
@@ -6,27 +6,59 @@ public class Solution10 {
     
     public static void printSolution() {
         System.out.println(calculate(256, 63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24));
-        System.out.println(calculate2(256, "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24"));
+        System.out.println(calculate2(256, "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24", 17, 31, 73, 47, 23));
     }
 
+    static int position = 0;
+    static int skip = 0;
+    
     public static int calculate(int length, int... input) {
         int[] string = initString(length);
         
+        boolean lastDebug = debug;
         debug = false;
+        position = 0;
+        skip = 0;
         hashRound(string, input);
-        debug = true;
+        debug = lastDebug;
         
         return string[0]*string[1];
     }
 
-    public static int calculate2(int length, String input) {
-        return 0;
+    public static String calculate2(int length, String inputString, int... inputExtra) {
+        int[] input = convertInput(inputString, inputExtra);
+        int[] string = initString(length);
+        position = 0;
+        skip = 0;
+        for (int i = 0; i < 64; ++i) {
+            hashRound(string, input);
+        }
+        int[] dense = reduce(string, 16);
+        String hexDense = convertHex(dense);
+        return hexDense;
     }
     
+    private static String convertHex(int[] dense) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < dense.length; ++i) {
+            if (dense[i] < 16) result.append("0");
+            result.append(Integer.toHexString(dense[i]));
+        }
+        return result.toString();
+    }
+
+    private static int[] reduce(int[] string, int groupSize) {
+        int[] result = new int[string.length/groupSize]; 
+        for (int i = 0; i < result.length; ++i) {
+            for (int j = 0; j < groupSize; ++j) {
+                result[i] ^= string[i*groupSize+j];
+            }
+        }
+        return result;
+    }
+
     private static void hashRound(int[] string, final int[] input) {
         int length = string.length;
-        int position = 0;
-        int skip = 0;
         
         print(position, string);
         for (int i = 0; i < input.length; ++i) {
@@ -58,6 +90,22 @@ public class Solution10 {
         System.out.println(result.toString());
     }
    
+    private static int[] convertInput(String inputString, int... inputExtra) {
+        char[] inputChars = inputString.toCharArray();
+        int[] input = new int[inputChars.length+inputExtra.length];
+        if (debug) System.out.print("input: ");
+        for (int i = 0; i < inputChars.length; ++i) {
+            input[i] = inputChars[i];
+            if (debug) System.out.print(input[i]+" ");
+        }
+        for (int i = 0; i < inputExtra.length; ++i) {
+            input[i+inputChars.length] = inputExtra[i];
+            if (debug) System.out.print(input[i+inputChars.length]+" ");
+        }
+        if (debug) System.out.println();
+        return input;
+    }
+
     private static int[] initString(int length) {
         int[] string = new int[length];
         for (int i = 0; i < length; ++i) {
--- src/hu/fallen/adventofcode/tests/Solution10Test.java
+++ src/hu/fallen/adventofcode/tests/Solution10Test.java
@@ -9,9 +9,13 @@ import hu.fallen.adventofcode.solutions.Solution10;
 public class Solution10Test {
     
     @Test
-    public void test() {
+    public void testCalculate() {
         assertEquals(12, Solution10.calculate(5, 3, 4, 1, 5));
         assertEquals(4480, Solution10.calculate(256, 63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24));
     }
 
+    @Test
+    public void testCalculate2() {
+        assertEquals("c500ffe015c83b60fad2e4b7d59dabc4", Solution10.calculate2(256, "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24", 17, 31, 73, 47, 23));
+    }
 }
