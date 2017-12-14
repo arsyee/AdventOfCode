6700d9c3e0908f5f24bb998bffa08938c314a192
arsyee <2476948+arsyee@users.noreply.github.com>
2017-12-14 16:14:46 +0100

    Update Solution10Test.java

--- src/hu/fallen/adventofcode/tests/Solution10Test.java
+++ src/hu/fallen/adventofcode/tests/Solution10Test.java
@@ -17,5 +17,6 @@ public class Solution10Test {
     @Test
     public void testCalculate2() {
         assertEquals("c500ffe015c83b60fad2e4b7d59dabc4", Solution10.calculate2(256, "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24", 17, 31, 73, 47, 23));
+        assertEquals("c500ffe015c83b60fad2e4b7d59dabc4", Solution10.knot("63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24"));
     }
 }
