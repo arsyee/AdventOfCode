532009fa5d1dc11182840e2e9f1a4a30374d2ecb
arsyee <2476948+arsyee@users.noreply.github.com>
2017-12-14 16:16:48 +0100

    Update Solution12.java

--- src/hu/fallen/adventofcode/solutions/Solution12.java
+++ src/hu/fallen/adventofcode/solutions/Solution12.java
@@ -13,7 +13,7 @@ import hu.fallen.adventofcode.helper.Pair.IntPair;
 
 public class Solution12 {
 
-	static boolean debug = false;
+	public static boolean debug = false;
 	
     public static void printSolution() {
         ArrayList<String> input;
@@ -28,6 +28,11 @@ public class Solution12 {
 
 	public static IntPair calculate(List<String> input) {
 		HashMap<Integer, HashSet<Integer>> sections = parse(input);
+		reduceEquivalenceClasses(sections);
+		return new IntPair(sections.get(new Integer(0)).size(), sections.size());
+	}
+
+	public static void reduceEquivalenceClasses(HashMap<Integer, HashSet<Integer>> sections) {
 		int size = 0;
 		eliminate: do {
 			print(sections);
@@ -35,7 +40,7 @@ public class Solution12 {
 			for (Entry<Integer, HashSet<Integer>> section : sections.entrySet()) {
 				for (Integer value : section.getValue()) {
 					if (!section.getKey().equals(value) && sections.keySet().contains(value)) {
-						System.out.println("moving "+value+" to "+section.getKey());
+						if (debug) System.out.println("moving "+value+" to "+section.getKey());
 						section.getValue().addAll(sections.get(value));
 						sections.remove(value);
 						continue eliminate;
@@ -43,7 +48,6 @@ public class Solution12 {
 				}
 			}
 		} while (size > sections.size());
-		return new IntPair(sections.get(new Integer(0)).size(), sections.size());
 	}
 
 	private static HashMap<Integer, HashSet<Integer>> parse(List<String> lines) {
@@ -62,7 +66,7 @@ public class Solution12 {
 		return result;
 	}
 
-	private static void print(HashMap<Integer, HashSet<Integer>> sections) {
+	static void print(HashMap<Integer, HashSet<Integer>> sections) {
 		if (!debug) return;
 		System.out.println("print entered:");
 		for (Entry<Integer, HashSet<Integer>> section : sections.entrySet()) {
