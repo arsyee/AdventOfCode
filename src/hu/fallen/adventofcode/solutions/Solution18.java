45964b733c2862f64290a3a247c563e6d9e875bf
bti2bp <2476948+arsyee@users.noreply.github.com>
2017-12-20 12:10:33 +0100

    Solution18

--- src/hu/fallen/adventofcode/solutions/Solution18.java
+++ src/hu/fallen/adventofcode/solutions/Solution18.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution18 {
 
@@ -20,10 +22,88 @@ public class Solution18 {
         System.out.println(calculate2(input));
     }
 
-	public static int calculate(ArrayList<String> input) {
-		return 0;
+	public static long calculate(ArrayList<String> input) {
+		Registers registers = new Registers();
+		long lastSound = 0;
+		int i = 0;
+		long x, y;
+		while (i < input.size()) {
+			final String line = input.get(i);
+			System.out.print("Processing line "+i+": "+line+": ");
+			String[] tokens = line.split(" ");
+			switch (tokens[0]) {
+				case "snd":
+					lastSound = registers.getValue(tokens[1]);
+					System.out.println("value saved: "+lastSound);
+					break;
+				case "set":
+					y = registers.getValue(tokens[2]);
+					registers.put(tokens[1].charAt(0), y);
+					break;
+				case "add":
+					x = registers.getValue(tokens[1]);
+					y = registers.getValue(tokens[2]);
+					registers.put(tokens[1].charAt(0), x + y);
+					break;
+				case "mul":
+					x = registers.getValue(tokens[1]);
+					y = registers.getValue(tokens[2]);
+					registers.put(tokens[1].charAt(0), x * y);
+					break;
+				case "mod":
+					x = registers.getValue(tokens[1]);
+					y = registers.getValue(tokens[2]);
+					if (y == 0) {
+						System.out.println("division by zero: "+line);
+						break;
+					}
+					registers.put(tokens[1].charAt(0), x % y);
+					break;
+				case "rcv":
+					x = registers.getValue(tokens[1]);
+					if (x == 0) {
+						System.out.println("rcv failed");
+						break;
+					}
+					if (lastSound != 0) return lastSound;
+				case "jgz":
+					x = registers.getValue(tokens[1]);
+					y = registers.getValue(tokens[2]);
+					if (x <= 0) {
+						System.out.println("jump failed");
+						break;
+					}
+					i = i + (int)y;
+					System.out.println("jumping to "+i);
+					continue;
+			}
+			++i;
+		}
+		throw new RuntimeException();
 	}
 
+    private static class Registers extends HashMap<Character, Long> {
+		private static final long serialVersionUID = 1L;
+		
+		long lastSound = 0;
+
+		@Override
+    	public Long put(Character key, Long value) {
+    		System.out.println(key + "->" + value);
+    		return super.put(key, value);
+    	}
+		
+		public long getValue(String string) {
+			try {
+				return Long.parseLong(string);
+			} catch (Exception e) { }
+			try {
+				return get(string.charAt(0));
+			} catch (Exception e) { }
+			return 0;
+		}
+    }
+    
 	public static int calculate2(ArrayList<String> input) {
 		return 0;
 	}
