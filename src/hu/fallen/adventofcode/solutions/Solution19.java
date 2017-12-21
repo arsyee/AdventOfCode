bac5b7ec739d5ef2455ee2849a42556a10f267e3
bti2bp <2476948+arsyee@users.noreply.github.com>
2017-12-21 10:59:02 +0100

    Day 19 solved

--- src/hu/fallen/adventofcode/solutions/Solution19.java
+++ src/hu/fallen/adventofcode/solutions/Solution19.java
@@ -5,7 +5,12 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.util.ArrayList;
 
+import hu.fallen.adventofcode.helper.Pair;
+import hu.fallen.adventofcode.helper.Pair.IntPair;
+
 public class Solution19 {
+	
+	private static final boolean DEBUG = true;
 
     public static void printSolution() {
     	String mark = new Object() {}.getClass().getEnclosingClass().getSimpleName().replace("Solution", "");
@@ -17,15 +22,150 @@ public class Solution19 {
             return;
         }
         System.out.println(calculate(input));
-        System.out.println(calculate2(input));
     }
 
-	public static int calculate(ArrayList<String> input) {
-		return 0;
+    public enum Dir {U, D, L, R};
+
+    static Dir dir = null;
+    static IntPair pos = null;
+    static char[][] map;
+    static int steps = 0;
+    
+	public static Pair<String, Integer> calculate(ArrayList<String> input) {
+		parseMap(input);
+		print();
+		
+		StringBuilder route = new StringBuilder();
+		dir = Dir.D;
+		pos = findEntrance();
+		
+		do {
+			switch (value()) {
+			case '|':
+			case '-':
+				step();
+				break;
+			case '+':
+				turn();
+				break;
+			default:
+				route.append(value());
+				step();
+				break;
+			}
+		} while (pos != null);
+		
+		return new Pair<>(route.toString(), steps);
 	}
 
-	public static int calculate2(ArrayList<String> input) {
-		return 0;
+	private static void turn() {
+		steps++;
+		int i = ((Integer)pos.get(0)).intValue();
+		int j = ((Integer)pos.get(1)).intValue();
+		switch (dir) {
+		case U:
+		case D:
+			pos = getNewpos(i, j-1);
+			dir = Dir.L;
+			if (pos != null) break;
+			pos = getNewpos(i, j+1);
+			dir = Dir.R;
+			break;
+		case L:
+		case R:
+			pos = getNewpos(i-1, j);
+			dir = Dir.U;
+			if (pos != null) break;
+			pos = getNewpos(i+1, j);
+			dir = Dir.D;
+			break;
+		}
+		debug("Turned to "+pos);
+	}
+
+	private static void step() {
+		steps++;
+		int i = ((Integer)pos.get(0)).intValue();
+		int j = ((Integer)pos.get(1)).intValue();
+		switch (dir) {
+		case U:
+			--i;
+			break;
+		case D:
+			++i;
+			break;
+		case L:
+			--j;
+			break;
+		case R:
+			++j;
+			break;
+		}
+		pos = getNewpos(i, j);
+		debug("Stepped to "+pos);
+	}
+
+	public static IntPair getNewpos(int i, int j) {
+		IntPair newpos = new IntPair(i, j);
+		try {
+			if (value(newpos) == ' ') { // route ends
+				return null;
+			} else {
+				return newpos;
+			}
+		} catch (Exception e) { // we left the map
+			return null;
+		}
+	}
+	
+	private static char value() {
+		return value(pos);
+	}	
+	
+	private static char value(IntPair pos) {
+		int i = ((Integer)pos.get(0)).intValue();
+		int j = ((Integer)pos.get(1)).intValue();
+		return map[i][j];
+	}
+
+	private static IntPair findEntrance() {
+		int x = -1;
+		for (int j = 0; j < map[0].length; ++j) {
+			if (map[0][j] == ' ') continue;
+			if (map[0][j] == '|' && x == -1) {
+				x = j;
+				continue;
+			}
+			throw new RuntimeException();
+		}
+		if (x == -1) throw new RuntimeException();
+		debug("Found entrance: 0,"+x);
+		return new IntPair(0, x);
+	}
+
+	private static void print() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < map.length; ++i) {
+			for (int j = 0; j < map[i].length; ++j) {
+				sb.append(map[i][j]);
+			}
+			sb.append("\n");
+		}
+		debug(sb.toString());
+	}
+
+	private static void parseMap(ArrayList<String> input) {
+		map = new char[input.size()][];
+		for (int i = 0; i < input.size(); ++i) {
+			map[i] = new char[input.get(i).length()];
+			for (int j = 0; j < input.get(i).length(); ++j) {
+				map[i][j] = input.get(i).charAt(j);
+			}
+		}
+	}
+
+	private static void debug(String string) {
+		if (DEBUG) System.out.println(string);
 	}
 
 }
--- src/hu/fallen/adventofcode/tests/Solution19Test.java
+++ src/hu/fallen/adventofcode/tests/Solution19Test.java
@@ -2,18 +2,28 @@ package hu.fallen.adventofcode.tests;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.util.ArrayList;
 
 import org.junit.Test;
 
+import hu.fallen.adventofcode.helper.Pair;
 import hu.fallen.adventofcode.solutions.Solution19;
 
 public class Solution19Test {
 	
 	@Test
 	public void test() {
-		assertEquals(0, Solution19.calculate(new ArrayList<String>()));
-		assertEquals(0, Solution19.calculate2(new ArrayList<String>()));
+        ArrayList<String> input;
+        try {
+            input = (ArrayList<String>) Files.readAllLines(FileSystems.getDefault().getPath("res", "test19.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        assertEquals(new Pair<>("ABCDEF", 38), Solution19.calculate(input));
 	}
 	
 }
